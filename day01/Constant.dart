6bb445284079b9ba9970af8fe4a6b92f4cf661bb
matrix12596 <1254966350@qq.com>
2026-03-02 21:06:23 +0800

    Constant

---
+++ day01/Constant.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  //常量，定义后不能改变
+  const PI = 3.1415926;
+  print(PI); //3.1415926
+
+  //注意：常量在定义时必须赋值，且不能改变
+  //const b;报错
+
+  //const表达式中不能有变量值
+  // int r=2;
+  // const  area=PI*r*r;//报错
+  // print(area);
+}
