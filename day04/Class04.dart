111c242050e404067e23bf26548388f32f643d93
matrix12596 <1254966350@qq.com>
2026-03-07 20:01:45 +0800

    Class04

---
+++ day04/Class04.dart
@@ -0,0 +1,38 @@
+void main(List<String> args) {
+  Person p = Person(name: "张三", age: 18, gender: "男");
+  p.study();
+
+  Person p2 = Person(name: "李四", age: 19, gender: "女");
+  p2.study();
+
+  Person p3 = Person.createPerson(name: "王五", age: 20, gender: "男");
+  p3.study();
+}
+
+class Person {
+  String? name = "";
+  int? age = 0;
+  String? gender = "男";
+
+  //默认构造函数
+  // Person({String? name, int? age, String? gender}) {
+  //   this.name = name;
+  //   this.age = age;
+  //   this.gender = gender;
+  // }
+
+  Person({this.age, this.name, this.gender});
+
+  //命名构造函数
+  // Person.createPerson({String? name, int? age, String? gender}) {
+  //   this.name = name;
+  //   this.age = age;
+  //   this.gender = gender;
+  // }
+
+  Person.createPerson({this.age, this.name, this.gender});
+
+  void study() {
+    print("${name}正在学习");
+  }
+}
