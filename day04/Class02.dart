e4f4d86ba80683ac35de132d208af41805347402
matrix12596 <1254966350@qq.com>
2026-03-07 20:00:19 +0800

    Class02

---
+++ day04/Class02.dart
@@ -0,0 +1,26 @@
+void main(List<String> args) {
+  Person p = Person(name: "张三", age: 18, gender: "男");
+  p.study();
+
+  Person p2 = Person(name: "李四", age: 20, gender: "女");
+  p2.study();
+
+  Person p3 = Person();
+  p3.study();
+}
+
+class Person {
+  String? name = "";
+  int? age = 0;
+  String? gender = "男";
+
+  //默认构造函数
+  Person({String? name, int? age, String? gender}) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+  void study() {
+    print("${name}正在学习");
+  }
+}
